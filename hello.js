8056e32820160e1ead19f2e26d311f3fe3406fe4
Dave Winer <dave.winer@gmail.com>
2017-08-29 19:30:02 -0400

    v0.4.0

--- hello.js
+++ hello.js
@@ -1,11 +1,8 @@
 var myVersion = "0.4.0", myProductName = "chokidarHelloWorld";  
-
 const chokidar = require ("chokidar");
-
 let watcher = chokidar.watch ("watch/", {
 	ignoreInitial: true
 	});
-
 watcher.on ("all", function (event, f) {
 	console.log ("event == " + event + ", f == " + f);
 	});
